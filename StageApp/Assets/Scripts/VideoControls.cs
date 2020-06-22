5f2e32178e47a217a6c9b430609454d02c666beb
Tycho <t.charko@outlook.com>
2020-06-22 10:30:49 +0200

    Add Project

---
+++ StageApp/Assets/Scripts/VideoControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoControls : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    private void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+    public void PlayVideo()
+    {
+        videoPlayer.Play();
+    }
+    public void PauzeVideo()
+    {
+        videoPlayer.Pause();
+    }
+    public void StopVideo()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+}
